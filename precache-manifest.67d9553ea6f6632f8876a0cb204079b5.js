15e78934feab301b43834ae75a43840c04596e79
Noel Nevarez <noel3740@gmail.com>
2019-03-04 16:27:00 -0600

    Updates

---
+++ precache-manifest.67d9553ea6f6632f8876a0cb204079b5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c2cfaeb1cf53e4602d06dd7c89a0ac5",
+    "url": "/clicky-game/static/media/luigi.6c2cfaeb.png"
+  },
+  {
+    "revision": "972f3e88725074eafc34",
+    "url": "/clicky-game/static/css/main.8e596724.chunk.css"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/clicky-game/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "39cac033d022d0b83705e4e050832a13",
+    "url": "/clicky-game/static/media/logo.39cac033.png"
+  },
+  {
+    "revision": "e6052918daf30614859fe202b166546e",
+    "url": "/clicky-game/static/media/controller.e6052918.jpg"
+  },
+  {
+    "revision": "972f3e88725074eafc34",
+    "url": "/clicky-game/static/js/main.972f3e88.chunk.js"
+  },
+  {
+    "revision": "e7cde1cb8e00c5823c5d0e41656eed00",
+    "url": "/clicky-game/static/media/mario.e7cde1cb.jpg"
+  },
+  {
+    "revision": "26073be8ed061b7820c9c3961a2bea69",
+    "url": "/clicky-game/static/media/masterChief.26073be8.jpg"
+  },
+  {
+    "revision": "b465a2ecadf6e1f36309ee357373ff5d",
+    "url": "/clicky-game/static/media/megaMan.b465a2ec.jpg"
+  },
+  {
+    "revision": "3f0e8e88bfb26536c8984b77108f90b8",
+    "url": "/clicky-game/static/media/sonic.3f0e8e88.png"
+  },
+  {
+    "revision": "61dcaf712f7e1980a2687f3e20adb27f",
+    "url": "/clicky-game/index.html"
+  }
+];
