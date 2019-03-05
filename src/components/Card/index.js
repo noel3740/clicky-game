9cd120804435c936267fd585f026a3289defe146
Noel Nevarez <noel3740@gmail.com>
2019-03-04 19:02:30 -0600

    Adding wave effect to card.

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,7 +5,7 @@ const Card = props => {
 
     return (
         <div className="reactCard col s12 m6 l3" >
-            <div className="card hoverable" onClick= { () => props.onClickMethod(props.imageUrl) }>
+            <div className="card hoverable waves-effect waves-dark" onClick= { () => props.onClickMethod(props.imageUrl) }>
                 <div className="card-image">
                     <img className="reactCardImage" src={props.imageUrl} alt={props.imageText} />
                 </div>
